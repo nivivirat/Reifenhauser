13ae671c453fb50e3f9a814253f6c9f0087acb28
nivivirat <imniveditha1188@gmail.com>
2023-11-04 23:21:21 +0530

    contact page done

--- src/Components/ContactUs/ContactUs.jsx
+++ src/Components/ContactUs/ContactUs.jsx
@@ -1,97 +1,116 @@
 import ContactUsForm from "./ContactUsForm"
 import headOffice from '../../assets/Images/ContactUs/headOffice.svg'
 import phone from '../../assets/Images/ContactUs/phone.svg'
+import map from '../../assets/Images/ContactUs/map.svg'
+import bluebg from '../../assets/Images/ContactUs/bluebg.svg'
 import { Icon } from "@iconify/react"
+import GoogleMap from "./GoogleMap"
 
 export default function ContactUs() {
     return (
-        <div className="md:p-10 p-5">
+        <div className="lg:p-10 p-5">
 
-            <div className="flex flex-col gap-8 md:w-[45%]">
-                <div className="md:text-[20px] text-[16px]">
-                    <p className="md:block hidden text-primary font-semibold">Contact Us</p>
-                    <div className="md:hidden flex-row flex gap-1">
-                        <p className="text-[#999999]">Home-</p>
-                        <p className="text-primary font-semibold">Contact Us</p>
-                    </div>
-                </div>
+            <div className="flex flex-row lg:gap-[10%]">
 
-                <div>
-                    <div className="font-semibold flex flex-row gap-1 md:text-[30px] text-[24px]">
-                        <p className="text-black">Get in Touch</p>
-                        <p className="text-[#285196]">With Us</p>
-                    </div>
-                    <div className="md:w-[60%]">
-                        <p className="text-[18px]">Contact us right away if you want to embrace technology</p>
-                    </div>
-                </div>
-
-                <div className="md:hidden flex">
-                    <ContactUsForm />
-                </div>
-
-                <div className="grid md:grid-cols-1 grid-cols-2 gap-4">
-                    <div className="w-full md:w-2/5 lg:w-1/2">
-                        <div className="flex flex-row items-center gap-2">
-                            <img src={headOffice} alt="Head Office" className="w-8 h-8" />
-                            <p className="text-primary md:text-2xl font-semibold text-xl">Head Office</p>
+                <div className="flex flex-col gap-8 lg:w-[45%]">
+                    <div className="lg:text-[20px] text-[16px]">
+                        <p className="lg:block hidden text-primary font-semibold">Contact Us</p>
+                        <div className="lg:hidden flex-row flex gap-1">
+                            <p className="text-[#999999]">Home-</p>
+                            <p className="text-primary font-semibold">Contact Us</p>
                         </div>
-                        <p className="text-[12px] md:text-[16px]">229, Udyog Bhavan, Sonawala Road, Opp Bridge Warsi, Goregaon East, Mumbai, Maharashtra 400063, India</p>
                     </div>
 
-                    {/* only for mobile */}
-                    <div className="w-full md:w-3/5 lg:w-1/2 md:hidden block">
-                        <div className="flex flex-row items-center gap-2">
-                            <img src={phone} alt="Contact" className="w-8 h-8" />
-                            <p className="text-primary md:text-2xl font-semibold text-xl">Contact</p>
+                    <div>
+                        <div className="font-semibold flex flex-row gap-1 lg:text-[30px] text-[24px]">
+                            <p className="text-black">Get in Touch</p>
+                            <p className="text-[#285196]">With Us</p>
                         </div>
-                        <p>+91 932 233 84 51</p>
+                        <div className="lg:w-[60%]">
+                            <p className="text-[18px]">Contact us right away if you want to embrace technology</p>
+                        </div>
                     </div>
 
-                    {/* only for mobile */}
-                    <div className="grid md:grid-cols-2 grid-cols-2 gap-4">
-                        <div className="w-full md:w-3/5 lg:w-1/2 md:block hidden">
+                    <div className="lg:hidden flex">
+                        <ContactUsForm />
+                    </div>
+
+                    <div className="grid lg:grid-cols-1 grid-cols-2 gap-8">
+                        <div className="w-full lg:w-2/5 lg:w-1/2">
+                            <div className="flex flex-row items-center gap-2">
+                                <img src={headOffice} alt="Head Office" className="w-8 h-8" />
+                                <p className="text-primary lg:text-2xl font-semibold text-xl">Head Office</p>
+                            </div>
+                            <p className="text-[12px] lg:text-[16px]">229, Udyog Bhavan, Sonawala Road, Opp Bridge Warsi, Goregaon East, Mumbai, Maharashtra 400063, India</p>
+                        </div>
+
+                        {/* only for mobile */}
+                        <div className="w-full lg:w-3/5 lg:w-1/2 lg:hidden block">
                             <div className="flex flex-row items-center gap-2">
                                 <img src={phone} alt="Contact" className="w-8 h-8" />
-                                <p className="text-primary md:text-2xl font-semibold text-xl">Contact</p>
+                                <p className="text-primary lg:text-2xl font-semibold text-xl">Contact</p>
                             </div>
                             <p>+91 932 233 84 51</p>
                         </div>
 
-                        <div className="w-full md:w-3/5 lg:w-1/2">
-                            <div className="flex flex-row items-center gap-2">
-                                <img src={phone} alt="Email" className="w-8 h-8" />
-                                <p className="text-primary md:text-2xl font-semibold text-xl">Email</p>
+                        {/* only for mobile */}
+                        <div className="grid lg:grid-cols-2 grid-cols-2 gap-4">
+                            <div className="w-full lg:w-3/5 lg:w-1/2 lg:block hidden">
+                                <div className="flex flex-row items-center gap-2">
+                                    <img src={phone} alt="Contact" className="w-8 h-8" />
+                                    <p className="text-primary lg:text-2xl font-semibold text-xl">Contact</p>
+                                </div>
+                                <p>+91 932 233 84 51</p>
                             </div>
-                            <p>info@reifenhauserindia.com</p>
+
+                            <div className="w-full lg:w-3/5 lg:w-1/2">
+                                <div className="flex flex-row items-center gap-2">
+                                    <img src={phone} alt="Email" className="w-8 h-8" />
+                                    <p className="text-primary lg:text-2xl font-semibold text-xl">Email</p>
+                                </div>
+                                <p>info@reifenhauserindia.com</p>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    {/* States */}
+                    <div className="flex flex-col gap-4 lg:text-[18px] text-[12px] text-primary font-semibold">
+                        <div className="flex flex-row justify-between">
+                            <p>Mumbai</p>
+                            <p>Ahmedabad</p>
+                            <p>Bengaluru</p>
+                            <p>Chennai</p>
+                            <p>Delhi</p>
+                        </div>
+                        <div className="flex flex-row justify-between px-4">
+                            <p>Hyderabad</p>
+                            <p>Jammu</p>
+                            <p>Kanpur</p>
+                            <p>Kolkata</p>
+                            <p>Pune</p>
                         </div>
                     </div>
 
+
                 </div>
 
-                {/* States */}
-                <div className="flex flex-col gap-4 md:text-[18px] text-[12px] text-primary font-semibold">
-                    <div className="flex flex-row justify-between">
-                        <p>Mumbai</p>
-                        <p>Ahmedabad</p>
-                        <p>Bengaluru</p>
-                        <p>Chennai</p>
-                        <p>Delhi</p>
-                    </div>
-                    <div className="flex flex-row justify-between px-4">
-                        <p>Hyderabad</p>
-                        <p>Jammu</p>
-                        <p>Kanpur</p>
-                        <p>Kolkata</p>
-                        <p>Pune</p>
+                <div className="lg:absolute lg:block z-0 right-0 hidden w-full pt-[10px]">
+                    <div className="relative">
+                        <img src={bluebg} className="absolute z-0 h-[550px] right-0 top-0" alt="Background Image" />
+                        <div className="absolute w-1/2 z-10 top-10 right-0">
+                            <ContactUsForm />
+                        </div>
                     </div>
                 </div>
 
 
             </div>
 
-            <div>
-                {/* map */}
+            {/* google map */}
+            <div className="lg:p-10 p-2 lg:mt-16">
+                {/* <GoogleMap /> */}
+                <img src={map} className="rounded-lg"></img>
             </div>
         </div>
     )
--- src/Components/ContactUs/ContactUsForm.jsx
+++ src/Components/ContactUs/ContactUsForm.jsx
@@ -1,8 +1,108 @@
-export default function ContactUsForm()
-{
-    return(
-        <div>
-            form
+import { useState } from "react";
+
+export default function ContactUsForm() {
+    const [formData, setFormData] = useState({
+        firstName: "",
+        lastName: "",
+        phoneNumber: "",
+        email: "",
+        serviceInterestedIn: "",
+        message: "",
+    });
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Here you can handle the form submission, e.g., sending data to a server
+        console.log(formData);
+        // Reset the form after submission
+        setFormData({
+            firstName: "",
+            phoneNumber: "",
+            email: "",
+            message: "",
+        });
+    };
+
+    return (
+        <div className="lg:w-[90%] w-full h-full bg-white border border-[#a0a0a0] md:p-10 p-7 md:rounded-[30px] rounded-[20px]">
+            <form
+                onSubmit={handleSubmit}
+                    className="flex flex-col gap-4"
+            >
+                <div className="md:flex md:space-x-4">
+                    <div className="w-full">
+                        <label htmlFor="firstName">First Name*</label>
+                        <input
+                            type="text"
+                            id="firstName"
+                            name="firstName"
+                            placeholder="First Name"
+                            value={formData.firstName}
+                            onChange={handleInputChange}
+                            className="w-full border border-[#a0a0a0] rounded-md p-2"
+                        />
+                    </div>
+                </div>
+                <div className="md:flex flex justify-between md:space-x-4">
+                    <div className="md:w-1/2 w-[48%]">
+                        <label htmlFor="phoneNumber">Phone Number</label>
+                        <input
+                            type="tel"
+                            id="phoneNumber"
+                            name="phoneNumber"
+                            placeholder="Phone Number"
+                            value={formData.phoneNumber}
+                            onChange={handleInputChange}
+                            className="w-full border border-[#a0a0a0] rounded-md p-2"
+                        />
+                    </div>
+                    <div className="md:w-1/2 w-[48%]">
+                        <label htmlFor="email" className="">
+                            Email*
+                        </label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Email Address"
+                            value={formData.email}
+                            onChange={handleInputChange}
+                            className="w-full border border-[#a0a0a0] rounded-md p-2"
+                        />
+                    </div>
+                </div>
+
+                <div className="flex flex-col">
+                    <label htmlFor="message" className="text-left">
+                        Message*
+                    </label>
+                    <textarea
+                        id="message"
+                        name="message"
+                        placeholder="Write here"
+                        value={formData.message}
+                        onChange={handleInputChange}
+                        rows="5"
+                        className="md:w-full w-full border border-[#a0a0a0] rounded-md p-2"
+                    ></textarea>
+                </div>
+
+                <button
+                    type="submit"
+                    className="bg-[#285196] text-white px-4 py-2 w-full md:w-full rounded-[50px]"
+                >
+                    Submit
+                </button>
+            </form>
+
         </div>
-    )
+    );
 }
