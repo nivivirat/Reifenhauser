646060f31ee858251f9ec0ff09d0b5526a9b4e08
alexwrites07 <101046777+alexwrites07@users.noreply.github.com>
2023-12-18 11:40:00 +0530

    Update CardWithLink.jsx

--- src/Components/Testimonial/CardWithLink.jsx
+++ src/Components/Testimonial/CardWithLink.jsx
@@ -11,7 +11,7 @@ import './Test.css'
 
 export function CardWithLink({ description, buttonText, pos, kl, poss, buttonTextt }) {
   return (
-    <Card className="mt-12 w-96 h-96 border border-blue-500">
+    <Card className="mt-12 ml-2 mr-2 sm:w-72 lg:w-96 md:w-96 border border-blue-500">
       {description && (
         <CardBody>
           {/* Your existing SVG and Typography components */}
