1cc8147f10a70788b64e353ece8a7dd8ca5ebc59
alexwrites07 <101046777+alexwrites07@users.noreply.github.com>
2023-12-22 18:13:32 +0530

    Update slider.jsx

--- src/Components/Blog/slider.jsx
+++ src/Components/Blog/slider.jsx
@@ -1,19 +1,31 @@
-import useEmblaCarousel from "embla-carousel-react"
+import React, { useEffect } from "react";
+import useEmblaCarousel from "embla-carousel-react";
 
 const Slider = ({ children, options }) => {
-  // 2. initialize EmblaCarousel using the custom hook
-  const [emblaRef] = useEmblaCarousel({
+  // Initialize EmblaCarousel using the custom hook
+  const [emblaRef, embla] = useEmblaCarousel({
     slidesToScroll: 1,
     align: "start",
-    ...options
-  })
+    ...options,
+  });
+
+  useEffect(() => {
+    // If Embla instance exists, enable autoplay
+    if (embla) {
+      const intervalId = setInterval(() => {
+        embla.scrollNext();
+      }, 3000); // Adjust the interval as needed (e.g., 3000ms for 3 seconds)
+      
+      // Clear the interval when component is unmounted
+      return () => clearInterval(intervalId);
+    }
+  }, [embla]);
 
   return (
-    // 3. set ref as emblaRef.
-    // make sure we have overflow-hidden and flex so that it displays properly
     <div className="overflow-hidden" ref={emblaRef}>
       <div className="flex gap-10">{children}</div>
     </div>
-  )
-}
-export default Slider
+  );
+};
+
+export default Slider;
