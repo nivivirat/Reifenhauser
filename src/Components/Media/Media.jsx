a64f7427ac4dde60e8ee748eff08662a347f0614
alexwrites07 <101046777+alexwrites07@users.noreply.github.com>
2023-12-22 17:48:43 +0530

    Update Media.jsx

--- src/Components/Media/Media.jsx
+++ src/Components/Media/Media.jsx
@@ -110,7 +110,7 @@ export default function Media() {
         </Card></a>
       </div>
       <div className="text-3xl flex-flex media3">Let’s Connect</div>
-      <div className="text-[18px] flex-flex media4">Here’s what we've been up to recently.</div>
+      <div className="text-[18px] flex-flex media4 -mb-24">Here’s what we've been up to recently.</div>
       <br></br>
       <CardComponent>
       </CardComponent>
