2207605d86ced10d66197ea5487d220c9433e6d5
alexwrites07 <101046777+alexwrites07@users.noreply.github.com>
2023-12-22 16:00:42 +0530

    Update Slider.jsx

--- src/Components/Home/HomeComponents/HomeTestimonalCard/Slider.jsx
+++ src/Components/Home/HomeComponents/HomeTestimonalCard/Slider.jsx
@@ -1,19 +1,33 @@
-import useEmblaCarousel from "embla-carousel-react"
+import React, { useEffect } from "react";
+import useEmblaCarousel from "embla-carousel-react";
 
 const Slider = ({ children, options }) => {
-  // 2. initialize EmblaCarousel using the custom hook
-  const [emblaRef] = useEmblaCarousel({
+  // Initialize EmblaCarousel using the custom hook
+  const [emblaRef, emblaApi] = useEmblaCarousel({
     slidesToScroll: 1,
     align: "start",
-    ...options
-  })
+    ...options,
+  });
+
+  // Set up auto-scrolling
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (emblaApi) {
+        emblaApi.scrollNext();
+      }
+    }, 2000); // Adjust the interval duration (in milliseconds) as needed
+
+    // Clear the interval on component unmount
+    return () => clearInterval(intervalId);
+  }, [emblaApi]);
 
   return (
-    // 3. set ref as emblaRef.
-    // make sure we have overflow-hidden and flex so that it displays properly
+    // Set ref as emblaRef.
+    // Make sure we have overflow-hidden and flex so that it displays properly
     <div className="overflow-hidden" ref={emblaRef}>
       <div className="flex gap-10">{children}</div>
     </div>
-  )
-}
-export default Slider
+  );
+};
+
+export default Slider;
