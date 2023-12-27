09b880981c009ea197ac069c1978c2c9ead47fc0
alexwrites07 <101046777+alexwrites07@users.noreply.github.com>
2023-12-27 18:18:04 +0530

    Update HomeTestimonials.jsx

--- src/Components/Home/HomeComponents/HomeTestimonials.jsx
+++ src/Components/Home/HomeComponents/HomeTestimonials.jsx
@@ -1,7 +1,6 @@
 import cardContent from '../../Testimonial/Content.json'
 import PropTypes from 'prop-types';
 import { Icon } from '@iconify/react';
-import { useState } from 'react';
 // import  { HomeTestimonialCard }  from './HomeTestimonalCard/HomeTestimonialCard';
 import Slider from './HomeTestimonalCard/Slider';
 import { CWL } from '../../Testimonial/CWL';
@@ -16,25 +15,21 @@ import {
 
 // import { CardWithLink } from '../../Testimonial/CardWithLink';
 export default function HomeTestimonials() {
-
-    const [isHovered, setIsHovered] = useState(false);
     // Use slice to get the first 3 items in the array
-    const first3Cards = cardContent.slice(0, 6);
+    const first3Cards = cardContent.slice(0, 3);
 
     return (
         <div className="bg-base flex flex-col">
-            <div className="md:px-[6%] pt-10 p-5 font-semibold md:w-[80%] xl:text-4xl text-2xl xl:font-semibold md:font-bold md:text-4xl w-full">
-                <span>Here's what our </span>
-                <span className="text-primary">customers </span>
-                <span>say </span>
-                <div className='pt-4'>about us</div>
+            <div className="md:px-[3%] pt-10 font-semibold md:w-[50%] xl:text-3xl text-2xl xl:font-semibold md:font-bold md:text-4xl w-full">
+            <span>Here's what our <span className="text-primary">customers</span> say about us</span>
+
             </div>
 
-            <div className="text-[#6B6B78] text-xs">
+            <div className="text-[#6B6B78] text-xs ">
            <Slider options={{ align: "center" }}>
           
-            {first3Cards.map((card, index) => (
-                 <div key={index} className="flex-[0_0_70%] md:flex-[0_0_30%]">
+            {cardContent.map((card, index) => (
+                 <div key={index} className="flex-[0_0_80%] md:flex-[0_0_30%] ">
                  <div >
           <CWL
             key={index}
@@ -52,13 +47,10 @@ export default function HomeTestimonials() {
         </Slider>
             </div>
 
-            <div className="w-full flex justify-center place-items-center mt-10 md:mb-10"
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}>
-                <a href="/testimonials" className="text-primary flex flex-row gap-2 p-3 md:rounded-lg rounded-2xl justify-center place-items-center font-medium border border-primary">
+            <div className="w-full flex justify-center place-items-center mt-10 md:mb-10">
+                <a href="/testimonials" className="text-primary flex flex-row gap-2 p-3 md:rounded-lg rounded-2xl justify-center place-items-center font-medium border border-primary md:w-[17%] w-[60%]">
                     <p>View All Testimonials</p>
-                    <div className={`text-primary md:text-xl transition-transform duration-300 ease-in-out ${isHovered ? 'rotate-45' : ''
-                        }`}>
+                    <div className="text-primary md:text-xl">
                         <Icon icon="iconoir:arrow-tr" />
                     </div>
                 </a>
