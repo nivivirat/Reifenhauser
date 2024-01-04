ffb265f094a0f6d27d9303cd4f6259d85356cb3c
alexwrites07 <101046777+alexwrites07@users.noreply.github.com>
2024-01-04 23:40:38 +0530

    Update CWL.jsx

--- src/Components/Testimonial/CWL.jsx
+++ src/Components/Testimonial/CWL.jsx
@@ -11,33 +11,33 @@ import './Test.css'
 
 export function CWL({ description, buttonText, pos, kl, poss, buttonTextt }) {
   return (
-    <Card className="mt-2 w-128 h-80 lg:h-56 md:h-80 border border-blue-500 vvvv">
+    <Card className="mt-2 w-128 h-80 lg:h-56 md:h-80 border  border-blue-500 vvvv">
       {description && (
         <CardBody>
           {/* Your existing SVG and Typography components */}
           
-          <Typography className="text-left">{description}</Typography>
+          <Typography className="text-black text-xs">{description}</Typography>
         </CardBody>
       )}
       <CardFooter className="pt-0">
         <a href="#" className="inline-block">
-        <Typography variant="text" className="flex items-center gap-2 test1">
+        <Typography variant="text" className="flex items-center gap-2 test1 text-[#013a98]">
             {buttonText}
             {/* Your existing SVG component */}
           </Typography>
-          <Typography variant="text" className="flex items-center gap-2 test90">
+          <Typography variant="text" className="flex items-center gap-2 test90 text-[#013a98]">
             {buttonTextt}
             {/* Your existing SVG component */}
           </Typography>
-      <Typography variant="text" className="flex items-left gap-2 test2">
+      <Typography variant="text" className="flex items-left gap-2 test2 text-[#013a98]">
             {pos}
             
           </Typography>
-          <Typography variant="text" className="flex items-right gap-2 test3">
+          <Typography variant="text" className="flex items-right gap-2 test3 text-[#013a98]">
             {kl}
             
           </Typography>
-          <Typography variant="text" className="flex items-right gap-2 test4">
+          <Typography variant="text" className="flex items-right gap-2 test4 text-[#013a98]">
             {poss}
             
           </Typography>
