b2ea8d8cba8208700828e60a262f4d7bee0533f4
alexwrites07 <101046777+alexwrites07@users.noreply.github.com>
2024-01-05 00:47:48 +0530

    Update CardWithLink.jsx

--- src/Components/Testimonial/CardWithLink.jsx
+++ src/Components/Testimonial/CardWithLink.jsx
@@ -7,46 +7,35 @@ import {
   Typography,
   Button,
 } from "@material-tailwind/react";
-import './Test.css'
 
 export function CardWithLink({ description, buttonText, pos, kl, poss, buttonTextt }) {
   return (
-    <Card className="mt-12 mb-10 ml-2 mr-2 sm:w-72 lg:w-96 md:w-96 border border-blue-500">
+    <Card className="mt-12 mb-10 ml-2 mr-2 sm:w-72 lg:w-96 md:w-96 border border-blue-500 ">
       {description && (
         <CardBody>
           {/* Your existing SVG and Typography components */}
-          
           <Typography className="text-left text-black text-2xs">{description}</Typography>
         </CardBody>
       )}
-      <CardFooter className="pt-0">
+      <CardFooter className="pt-0 text-blue">
         <a href="#" className="inline-block">
-        <Typography variant="text" className="flex items-center gap-2 text-[#013a98] test1">
+          <Typography variant="text" style={{ color: '#013A98' }} className="flex items-center gap-2 test1">
             {buttonText}
-            {/* Your existing SVG component */}
           </Typography>
-          <Typography variant="text" className="flex items-center gap-2 text-[#013a98] test90 ">
+          <Typography variant="text" style={{ color: '#013A98' }} className="flex items-center gap-2 test90">
             {buttonTextt}
-            {/* Your existing SVG component */}
           </Typography>
-      <Typography variant="text" className="flex items-left gap-2 text-[#013a98] test2">
+          <Typography variant="text" style={{ color: '#013A98' }} className="flex items-left gap-2 test2">
             {pos}
-            
           </Typography>
-          
-          <Typography variant="text" className="flex items-right gap-2 text-[#013a98] test3">
+          <Typography variant="text" style={{ color: '#013A98' }} className="flex items-right gap-2 test3">
             {kl}
-            
           </Typography>
-          
-          <Typography variant="text" className="flex items-right gap-2 text-[#013a98] test4">
+          <Typography variant="text" style={{ color: '#013A98' }} className="flex items-right gap-2 test4">
             {poss}
-            
           </Typography>
-          
         </a>
       </CardFooter>
-    
     </Card>
   );
 }
