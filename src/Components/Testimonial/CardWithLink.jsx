1a55a1cb417be917d3b57ee03dbd8cd1dc10dc42
alexwrites07 <101046777+alexwrites07@users.noreply.github.com>
2024-01-09 23:30:50 +0530

    Update CardWithLink.jsx

--- src/Components/Testimonial/CardWithLink.jsx
+++ src/Components/Testimonial/CardWithLink.jsx
@@ -1,58 +1,43 @@
+// CardWithLink.jsx
 import React from 'react';
 import PropTypes from 'prop-types';
-import {
-  Card,
-  CardBody,
-  CardFooter,
-  Typography,
-  Button,
-} from "@material-tailwind/react";
-import './Test.css'
+import { Card, CardBody, CardFooter, Typography } from "@material-tailwind/react";
 
-export function CardWithLink({ description, buttonText, pos, kl, poss, buttonTextt }) {
+export function CardWithLink({ title, testimonial }) {
   return (
-    <Card className="mt-12 mb-10 ml-2 mr-2 sm:w-72 lg:w-96 md:w-96 border border-blue-500">
-      {description && (
-        <CardBody>
-          {/* Your existing SVG and Typography components */}
-          
-          <Typography className="text-left text-black text-2xs">{description}</Typography>
-        </CardBody>
-      )}
-      <CardFooter className="pt-0">
+    <Card className="mt-12 mb-10 ml-2 mr-2 sm:w-80 lg:w-96 md:w-96 border border-blue-500">
+      <CardBody>
+        <Typography className="text-left text-black text-2xs">
+          {testimonial.description}
+        </Typography>
+      </CardBody>
+      <CardFooter className="pt-0 text-blue">
         <a href="#" className="inline-block">
-        <Typography variant="text" className="flex items-center gap-2 text-[#013a98] test1">
-            {buttonText}
-            {/* Your existing SVG component */}
+          <Typography variant="text" style={{ color: '#013A98' }} className="flex items-center gap-2 test90">
+            {testimonial.buttonText}
           </Typography>
-          <Typography variant="text" className="flex items-center gap-2 text-[#013a98] test90 ">
-            {buttonTextt}
-            {/* Your existing SVG component */}
+          <Typography variant="text" style={{ color: '#013A98' }} className="flex items-left gap-2 mt-8 test2">
+            {testimonial.pos}
           </Typography>
-      <Typography variant="text" className="flex items-left gap-2 text-[#013a98] test2">
-            {pos}
-            
+          <Typography variant="text" style={{ color: '#013A98' }} className="flex items-right gap-2 -mb-1 test3">
+            {testimonial.kl}
           </Typography>
-          
-          <Typography variant="text" className="flex items-right gap-2 text-[#013a98] test3">
-            {kl}
-            
+          <Typography variant="text" style={{ color: '#013A98' }} className="flex items-right gap-2 test4">
+            {testimonial.poss}
           </Typography>
-          
-          <Typography variant="text" className="flex items-right gap-2 text-[#013a98] test4">
-            {poss}
-            
-          </Typography>
-          
         </a>
       </CardFooter>
-    
     </Card>
   );
 }
 
 CardWithLink.propTypes = {
   title: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
-  buttonText: PropTypes.string.isRequired,
+  testimonial: PropTypes.shape({
+    description: PropTypes.string,
+    buttonText: PropTypes.string,
+    pos: PropTypes.string,
+    kl: PropTypes.string,
+    poss: PropTypes.string,
+  }).isRequired,
 };
