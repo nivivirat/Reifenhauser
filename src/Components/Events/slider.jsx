5a5d8a6a1428f25f2e962ce0f302709fa9d12817
alexwrites07 <101046777+alexwrites07@users.noreply.github.com>
2024-01-11 20:58:53 +0530

    Update slider.jsx

--- src/Components/Events/slider.jsx
+++ src/Components/Events/slider.jsx
@@ -1,22 +1,58 @@
-import React from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import "./Events.css"; // Import your custom styles
+import React, { useEffect } from "react";
+import useEmblaCarousel from "embla-carousel-react";
 
-const CustomSlider = ({ children }) => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
+const Slider = ({ children, options }) => {
+  // Initialize EmblaCarousel using the custom hook
+  const [emblaRef, embla] = useEmblaCarousel({
     slidesToScroll: 1,
-    swipeToSlide: true,
-    touchMove: true,
-    draggable: true,
-  };
+    align: "start",
+    loop: true, // Enable loop option
+    ...options,
+  });
 
-  return <Slider {...settings}>{children}</Slider>;
+  useEffect(() => {
+    let intervalId;
+
+    const handlePointerDown = () => {
+      // Pause auto-scrolling when the user touches any card
+      clearInterval(intervalId);
+    };
+
+    const handlePointerUp = () => {
+      // Resume auto-scrolling when the user releases the touch
+      startAutoScroll();
+    };
+
+    const startAutoScroll = () => {
+      intervalId = setInterval(() => {
+        if (embla) {
+          embla.scrollNext();
+        }
+      }, 5000); // Adjust the interval as needed (e.g., 3000ms for 3 seconds)
+    };
+
+    // If Embla instance exists, enable autoplay
+    if (embla) {
+      startAutoScroll();
+
+      // Add event listeners for touch events
+      embla.on("pointerDown", handlePointerDown);
+      embla.on("pointerUp", handlePointerUp);
+
+      // Clear the interval and remove the event listeners when the component is unmounted
+      return () => {
+        clearInterval(intervalId);
+        embla.off("pointerDown", handlePointerDown);
+        embla.off("pointerUp", handlePointerUp);
+      };
+    }
+  }, [embla]);
+
+  return (
+    <div className="overflow-hidden" ref={emblaRef}>
+      <div className="flex ">{children}</div>
+    </div>
+  );
 };
 
-export default CustomSlider;
+export default Slider;
