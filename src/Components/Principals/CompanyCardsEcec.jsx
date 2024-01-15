956b80d63a50f3f74035e3b1ea3ae136f24c07f4
nivivirat <imniveditha1188@gmail.com>
2024-01-15 08:58:58 +0530

    img bug resolved in principals

--- src/Components/Principals/CompanyCardsEcec.jsx
+++ src/Components/Principals/CompanyCardsEcec.jsx
@@ -1,92 +1,13 @@
 import React, { useState } from 'react';
-
-// CompanyLogoExec
-
-import asano from '../../assets/Images/Principals/CompanyLogoExec/asano.svg';
-import blesson from '../../assets/Images/Principals/CompanyLogoExec/blesson.svg';
-import buss from '../../assets/Images/Principals/CompanyLogoExec/buss.svg';
-import fimic from '../../assets/Images/Principals/CompanyLogoExec/fimic.svg';
-import herbold from '../../assets/Images/Principals/CompanyLogoExec/herbold.svg';
-import kurre from '../../assets/Images/Principals/CompanyLogoExec/kurre.svg';
-import horauf from '../../assets/Images/Principals/CompanyLogoExec/horauf.svg';
-import ngr from '../../assets/Images/Principals/CompanyLogoExec/ngr.svg';
-
-// ConvertingMachinary
-
-import comexi from '../../assets/Images/Principals/ConvertingMachinary/comexi.svg';
-import energy_sciences from '../../assets/Images/Principals/ConvertingMachinary/energy_sciences.svg';
-import lang_laser from '../../assets/Images/Principals/ConvertingMachinary/lang_laser.svg';
-import pelican_rotoflex from '../../assets/Images/Principals/ConvertingMachinary/pelican_rotoflex.svg';
-import totani from '../../assets/Images/Principals/ConvertingMachinary/totani.svg';
-import coatema from '../../assets/Images/Principals/ConvertingMachinary/coatema.svg';
-
-// Ancillary
-
-import acigraf from '../../assets/Images/Principals/Ancillary/acigraf.svg';
-import av_flexologic from '../../assets/Images/Principals/Ancillary/av_flexologic.svg';
-import esko from '../../assets/Images/Principals/Ancillary/esko.svg';
-import mero_spa from '../../assets/Images/Principals/Ancillary/mero_spa.svg';
-import praxair from '../../assets/Images/Principals/Ancillary/praxair.svg';
-import reprochem from '../../assets/Images/Principals/Ancillary/reprochem.svg';
-import synaptik_lamination from '../../assets/Images/Principals/Ancillary/synaptik_lamination.svg';
-import lmg from '../../assets/Images/Principals/Ancillary/lmg.svg';
-
-// SMC
-
-import acelli from '../../assets/Images/Principals/SMC/acelli.svg'
-import buhler from '../../assets/Images/Principals/SMC/buhler.svg'
-import ims from '../../assets/Images/Principals/SMC/ims.svg'
-import kroenert from '../../assets/Images/Principals/SMC/kroenert.svg'
-
-// labels
-
-import bobst from '../../assets/Images/Principals/Labels/bobst.svg';
-import gic from '../../assets/Images/Principals/Labels/gic.svg';
-import grafotronic from '../../assets/Images/Principals/Labels/grafotronic.svg';
-import konica_minolta from '../../assets/Images/Principals/Labels/konica_minolta.svg';
-
+import PropTypes from "prop-types";
 
 export default function CompanyCardsEcec({ companyName, img, backContent, back2, back3, backLink }) {
-    
+
     const [card, setCard] = useState(true);
 
-    function handleClick()
-    {
+    function handleClick() {
         setCard(!card);
     }
-
-    const images = {
-        asano,
-        blesson,
-        buss,
-        fimic,
-        herbold,
-        kurre,
-        horauf,
-        ngr,
-        bobst,
-        comexi,
-        energy_sciences,
-        gic,
-        konica_minolta,
-        lang_laser,
-        pelican_rotoflex,
-        totani,
-        acigraf,
-        av_flexologic,
-        esko,
-        grafotronic,
-        mero_spa,
-        praxair,
-        reprochem,
-        synaptik_lamination,
-        acelli,
-        buhler,
-        ims,
-        kroenert,
-        coatema,
-        lmg
-    };
 
     // const formattedLink = backLink.startsWith('http://') || backLink.startsWith('https://') ? backLink : `https://${backLink}`;
     const handleMouseEnter = () => {
@@ -109,7 +30,7 @@ export default function CompanyCardsEcec({ companyName, img, backContent, back2,
                 className='border border-[#b3b3b3] rounded-[20px] flex justify-center place-items-center content-center h-[110px]'
             >
                 {card ?
-                    <img src={images[img]} alt={companyName} className='w-24 h-auto' />
+                    <img src={img} alt={companyName} className='w-24 h-auto' />
                     :
                     <div className='w-full flex-col gap-3 bg-[#e5eaea] border border-[#b3b3b3] rounded-[20px] flex justify-center place-items-center content-center h-[110px]'>
                         <p className='text-[#013A98] text-center md:text-[10px] text-[9px] px-4 font-medium'>{backContent}</p>
@@ -126,3 +47,13 @@ export default function CompanyCardsEcec({ companyName, img, backContent, back2,
         </div>
     );
 }
+
+CompanyCardsEcec.propTypes = {
+    companyName: PropTypes.string.isRequired,
+    img: PropTypes.string.isRequired,
+    backContent: PropTypes.string.isRequired,
+    back2: PropTypes.string.isRequired,
+    back3: PropTypes.string.isRequired,
+    backLink: PropTypes.string.isRequired,
+};
+
