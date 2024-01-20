1cce2e696498645f6117dd790c0fd7d00bb529e3
alexwrites07 <101046777+alexwrites07@users.noreply.github.com>
2024-01-20 18:35:23 +0530

    Update CWL.jsx

--- src/Components/Testimonial/CWL.jsx
+++ src/Components/Testimonial/CWL.jsx
@@ -11,7 +11,7 @@ import './Test.css'
 
 export function CWL({ description, buttonText, pos, kl, poss, buttonTextt }) {
   return (
-    <Card className="mt-2 w-128 h-80 lg:h-64 md:h-80 border  border-blue-500 vvvv">
+    <Card className="mt-2 w-128 h-80 lg:h-56 md:h-80 border  border-blue-500 vvvv">
       {description && (
         <CardBody>
           {/* Your existing SVG and Typography components */}
@@ -21,23 +21,23 @@ export function CWL({ description, buttonText, pos, kl, poss, buttonTextt }) {
       )}
       <CardFooter className="pt-0">
         <a href="#" className="inline-block">
-        <Typography variant="text" style={{ color: '#013A98' }} className="flex items-center gap-2 -ml-40 test1 text-black">
+        <Typography variant="text" style={{ color: '#013A98', fontSize: '10px' }} className="flex items-center gap-2 -ml-40 test1 text-black">
             {buttonText}
             {/* Your existing SVG component */}
           </Typography>
-          <Typography variant="text" style={{ color: '#013A98' }} className="flex items-center gap-2 test90 text-[#013a98]">
+          <Typography variant="text" style={{ color: '#013A98', fontSize: '10px' }} className="flex items-center gap-2  test90 text-[#013a98]">
             {buttonTextt}
             {/* Your existing SVG component */}
           </Typography>
-      <Typography variant="text" style={{ color: '#013A98' }} className="flex items-left gap-2 test2 text-[#013a98]">
+      <Typography variant="text" style={{ color: '#013A98', fontSize: '10px' }} className="flex items-left gap-2 test2 text-[#013a98]">
             {pos}
             
           </Typography>
-          <Typography variant="text" style={{ color: '#013A98' }} className="flex items-right gap-2 test3 text-[#013a98]">
+          <Typography variant="text" style={{ color: '#013A98', fontSize: '10px' }} className="flex items-right gap-2 test3 text-[#013a98]">
             {kl}
             
           </Typography>
-          <Typography variant="text" style={{ color: '#013A98' }} className="flex items-right gap-2 test4 text-[#013a98]">
+          <Typography variant="text" style={{ color: '#013A98', fontSize: '10px' }} className="flex items-right gap-2 test4 text-[#013a98]">
             {poss}
             
           </Typography>
