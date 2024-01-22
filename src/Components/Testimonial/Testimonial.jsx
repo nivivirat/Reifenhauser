12b5e0f223457a76c86b5b710bb67d9625b6815d
nivivirat <imniveditha1188@gmail.com>
2024-01-22 18:20:07 +0530

    demo link added for firebase

--- src/Components/Testimonial/Testimonial.jsx
+++ src/Components/Testimonial/Testimonial.jsx
@@ -6,14 +6,24 @@ import { CardWithLink } from './CardWithLink';
 import { initializeApp } from 'firebase/app';
 import { getDatabase, ref, onValue } from 'firebase/database';
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDiNLjf19bW0-5cvkOtdlqYI7YiDzt3WA0",
+//   authDomain: "reifenhauser-2d366.firebaseapp.com",
+//   projectId: "reifenhauser-2d366",
+//   storageBucket: "reifenhauser-2d366.appspot.com",
+//   messagingSenderId: "1000320736803",
+//   appId: "1:1000320736803:web:c9db2603f14597edf45b96",
+//   measurementId: "G-80E388KDKZ",
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDiNLjf19bW0-5cvkOtdlqYI7YiDzt3WA0",
-  authDomain: "reifenhauser-2d366.firebaseapp.com",
-  projectId: "reifenhauser-2d366",
-  storageBucket: "reifenhauser-2d366.appspot.com",
-  messagingSenderId: "1000320736803",
-  appId: "1:1000320736803:web:c9db2603f14597edf45b96",
-  measurementId: "G-80E388KDKZ",
+  apiKey: "AIzaSyA1h6QXsmX4PcYCZILMTtry8UaaMbcBNKg",
+  authDomain: "agent-807a7.firebaseapp.com",
+  databaseURL: "https://agent-807a7-default-rtdb.firebaseio.com",
+  projectId: "agent-807a7",
+  storageBucket: "agent-807a7.appspot.com",
+  messagingSenderId: "918626263804",
+  appId: "1:918626263804:web:627f47367e84af8e5c2d79"
 };
 
 function Testimonial() {
@@ -33,7 +43,7 @@ function Testimonial() {
         onValue(testimonialsRef, (snapshot) => {
           const data = snapshot.val();
           const testimonialsArray = [];
-    
+
           for (const uid in data) {
             const testimonialData = data[uid];
             const testimonial = {
@@ -44,17 +54,17 @@ function Testimonial() {
               poss: testimonialData?.poss || '',
               description: testimonialData?.description || '',
             };
-    
+
             testimonialsArray.push(testimonial);
           }
-    
+
           setTestimonials(testimonialsArray);
         });
       } catch (error) {
         console.error('Error fetching testimonials:', error);
       }
     };
-    
+
 
     fetchTestimonials();
   }, []);
--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,26 @@
 import { initializeApp } from 'firebase/app';
 import { getDatabase, ref, get } from 'firebase/database';
 
+// const firebaseConfig = {
+//     apiKey: "AIzaSyDiNLjf19bW0-5cvkOtdlqYI7YiDzt3WA0",
+//     authDomain: "reifenhauser-2d366.firebaseapp.com",
+//     projectId: "reifenhauser-2d366",
+//     storageBucket: "reifenhauser-2d366.appspot.com",
+//     messagingSenderId: "1000320736803",
+//     appId: "1:1000320736803:web:c9db2603f14597edf45b96",
+//     measurementId: "G-80E388KDKZ",
+//   };
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDiNLjf19bW0-5cvkOtdlqYI7YiDzt3WA0",
-    authDomain: "reifenhauser-2d366.firebaseapp.com",
-    projectId: "reifenhauser-2d366",
-    storageBucket: "reifenhauser-2d366.appspot.com",
-    messagingSenderId: "1000320736803",
-    appId: "1:1000320736803:web:c9db2603f14597edf45b96",
-    measurementId: "G-80E388KDKZ",
-  };
+  apiKey: "AIzaSyA1h6QXsmX4PcYCZILMTtry8UaaMbcBNKg",
+  authDomain: "agent-807a7.firebaseapp.com",
+  databaseURL: "https://agent-807a7-default-rtdb.firebaseio.com",
+  projectId: "agent-807a7",
+  storageBucket: "agent-807a7.appspot.com",
+  messagingSenderId: "918626263804",
+  appId: "1:918626263804:web:627f47367e84af8e5c2d79"
+};
+
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
