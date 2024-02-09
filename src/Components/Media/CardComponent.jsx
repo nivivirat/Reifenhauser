893173147cb8efca784ec73cf08a72ecb5a2ecdd
nivivirat <imniveditha1188@gmail.com>
2024-02-09 19:00:32 +0530

    changes to main db

--- src/Components/Media/CardComponent.jsx
+++ src/Components/Media/CardComponent.jsx
@@ -8,14 +8,15 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase, ref, get } from 'firebase/database';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA1h6QXsmX4PcYCZILMTtry8UaaMbcBNKg",
-    authDomain: "agent-807a7.firebaseapp.com",
-    databaseURL: "https://agent-807a7-default-rtdb.firebaseio.com",
-    projectId: "agent-807a7",
-    storageBucket: "agent-807a7.appspot.com",
-    messagingSenderId: "918626263804",
-    appId: "1:918626263804:web:627f47367e84af8e5c2d79"
-  };
+    apiKey: "AIzaSyDiNLjf19bW0-5cvkOtdlqYI7YiDzt3WA0",
+    authDomain: "reifenhauser-2d366.firebaseapp.com",
+    databaseURL: "https://reifenhauser-2d366-default-rtdb.firebaseio.com",
+    projectId: "reifenhauser-2d366",
+    storageBucket: "reifenhauser-2d366.appspot.com",
+    messagingSenderId: "1000320736803",
+    appId: "1:1000320736803:web:c9db2603f14597edf45b96",
+    measurementId: "G-80E388KDKZ"
+};
 
 const firebaseApp = initializeApp(firebaseConfig);
 const database = getDatabase(firebaseApp);
@@ -58,14 +59,14 @@ export default function CardComponent() {
     return (
         <div className="w-screen fg flex flex-col md:gap-5 gap-[100px] pb-10 -mb-24 -mx-4 overflow-x-auto">
             <Slider options={{ align: "center" }}>
-            {mediaData.slice().reverse().map((mediaCard) => (
+                {mediaData.slice().reverse().map((mediaCard) => (
                     <div key={mediaCard.uid} className="flex-[0_0_90%] md:flex-[0_0_23%] mx-2">
                         <a href={mediaCard.link} target="_blank" rel="noopener noreferrer">
                             <div className="h-80 w-80 bg-white  rounded-lg my-40">
                                 <div className="flex justify-center md:justify-end -mt-16 relative">
                                     <div style={{ height: '100%', width: '100%' }} className="relative">
                                         <img
-                                         className="object-cover w-96 h-80 object-cover"
+                                            className="object-cover w-96 h-80 object-cover"
                                             src={mediaCard.img}
                                             alt={mediaCard.title}
                                         />
@@ -77,13 +78,13 @@ export default function CardComponent() {
                                             rel="noopener noreferrer"  // Recommended for security reasons when using target="_blank"
                                         ></a>
 
-                                    
+
+                                    </div>
                                 </div>
-                            </div>
-                            <div>
-                                {/* Other content goes here */}
-                            </div>
-                        </div></a>
+                                <div>
+                                    {/* Other content goes here */}
+                                </div>
+                            </div></a>
                     </div>
                 ))}
             </Slider>
--- src/Components/Testimonial/Testimonial.jsx
+++ src/Components/Testimonial/Testimonial.jsx
@@ -6,24 +6,15 @@ import { CardWithLink } from './CardWithLink';
 import { initializeApp } from 'firebase/app';
 import { getDatabase, ref, onValue } from 'firebase/database';
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDiNLjf19bW0-5cvkOtdlqYI7YiDzt3WA0",
-//   authDomain: "reifenhauser-2d366.firebaseapp.com",
-//   projectId: "reifenhauser-2d366",
-//   storageBucket: "reifenhauser-2d366.appspot.com",
-//   messagingSenderId: "1000320736803",
-//   appId: "1:1000320736803:web:c9db2603f14597edf45b96",
-//   measurementId: "G-80E388KDKZ",
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyA1h6QXsmX4PcYCZILMTtry8UaaMbcBNKg",
-  authDomain: "agent-807a7.firebaseapp.com",
-  databaseURL: "https://agent-807a7-default-rtdb.firebaseio.com",
-  projectId: "agent-807a7",
-  storageBucket: "agent-807a7.appspot.com",
-  messagingSenderId: "918626263804",
-  appId: "1:918626263804:web:627f47367e84af8e5c2d79"
+  apiKey: "AIzaSyDiNLjf19bW0-5cvkOtdlqYI7YiDzt3WA0",
+  authDomain: "reifenhauser-2d366.firebaseapp.com",
+  databaseURL: "https://reifenhauser-2d366-default-rtdb.firebaseio.com",
+  projectId: "reifenhauser-2d366",
+  storageBucket: "reifenhauser-2d366.appspot.com",
+  messagingSenderId: "1000320736803",
+  appId: "1:1000320736803:web:c9db2603f14597edf45b96",
+  measurementId: "G-80E388KDKZ"
 };
 
 function Testimonial() {
