3d8d908cb167879ae94c1843bc16571bd414ca41
nivivirat <imniveditha1188@gmail.com>
2024-02-10 23:02:57 +0530

    routing test 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './Components/Header/Header'
-import Footer from './Components/Footer/Footer'
-import ContactUs from './Components/ContactUs/ContactUs';
-import Principals from './Components/Principals/Principals';
-import Services from './Components/Services/Services';
-import Media from './Components/Media/Media';
+// import { Route, HashRouter as Router, Routes } from 'react-router-dom';
 import AboutUs from './Components/AboutUs/AboutUs';
-import Home from './Components/Home/Home';
-import Events from './Components/Events/Events';
-import Testimonial from './Components/Testimonial/Testimonial';
+import Admin from './Components/Admin/Admin';
 import Blog from './Components/Blog/Blog';
 import Careers from './Components/Careers/Careers';
-import Admin from './Components/Admin/Admin';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import ContactUs from './Components/ContactUs/ContactUs';
+import Events from './Components/Events/Events';
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
+import Media from './Components/Media/Media';
+import Principals from './Components/Principals/Principals';
+import Services from './Components/Services/Services';
+import Testimonial from './Components/Testimonial/Testimonial';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
@@ -25,7 +25,6 @@ function App() {
           {/* <Route path="/" element={<Header />} /> */}
           <Route path="/" element={<Home />} />
           <Route path="/contactUs" element={<ContactUs />} />
-          <Route path="/Principals" element={<Principals />} />
           <Route path="/services" element={<Services />} />
           <Route path="/media" element={<Media />} />
           <Route path="/principals" element={<Principals />} />
